7a93d195185c3fcc0742d4e9a17f1d6dbafb902e
Juan Fumero <jjfumero@gmail.com>
2024-10-03 14:54:49 +0200

    Benchmark script improved

--- benchmark.sh
+++ benchmark.sh
@@ -2,28 +2,28 @@ logFolder="log_$(date +"%F_%H_%M_%S")"
 
 mkdir -p $logFolder
 ## Java
-bash butil.sh -v java stories15M.bin 2> $logFolder/log15_java
-bash butil.sh -v java stories42M.bin 2> $logFolder/log42_java
-bash butil.sh -v java stories110M.bin 2> $logFolder/log110_java
+bash butil.sh -v java stories15M.bin  2>&1 | tee -a $logFolder/log15_java
+bash butil.sh -v java stories42M.bin  2>&1 | tee -a $logFolder/log42_java
+bash butil.sh -v java stories110M.bin 2>&1 | tee -a $logFolder/log110_java
 
 ## Level Zero Device 0
-bash butil.sh -v levelzero -d 0 stories15M.bin 2> $logFolder/log15_levelzero_0
-bash butil.sh -v levelzero -d 0 stories42M.bin 2> $logFolder/log42_levelzero_0
-bash butil.sh -v levelzero -d 0 stories110M.bin 2> $logFolder/log110_levelzero_0
+bash butil.sh -v levelzero -d 0 stories15M.bin 2>&1  | tee -a $logFolder/log15_levelzero_0
+bash butil.sh -v levelzero -d 0 stories42M.bin 2>&1  | tee -a $logFolder/log42_levelzero_0
+bash butil.sh -v levelzero -d 0 stories110M.bin 2>&1 | tee -a $logFolder/log110_levelzero_0
 
 ## Level Zero Device 1
-bash butil.sh -v levelzero -d 1 stories15M.bin 2> $logFolder/log15_levelzero_1
-bash butil.sh -v levelzero -d 1 stories42M.bin 2> $logFolder/log42_levelzero_1
-bash butil.sh -v levelzero -d 1 stories110M.bin 2> $logFolder/log110_levelzero_1
+bash butil.sh -v levelzero -d 1 stories15M.bin 2>&1  | tee -a $logFolder/log15_levelzero_1
+bash butil.sh -v levelzero -d 1 stories42M.bin 2>&1  | tee -a $logFolder/log42_levelzero_1
+bash butil.sh -v levelzero -d 1 stories110M.bin 2>&1 | tee -a $logFolder/log110_levelzero_1
 
 
 ## TornadoVM Device 0
-bash butil.sh -v tornadovm -d 0 stories15M.bin 2> $logFolder/log15_tornadovm_0
-bash butil.sh -v tornadovm -d 0 stories42M.bin 2> $logFolder/log42_tornadovm_0
-bash butil.sh -v tornadovm -d 0 stories110M.bin 2> $logFolder/log110_tornadovm_0
+bash butil.sh -v tornadovm -d 0 stories15M.bin 2>&1  | tee -a $logFolder/log15_tornadovm_0
+bash butil.sh -v tornadovm -d 0 stories42M.bin 2>&1  | tee -a $logFolder/log42_tornadovm_0
+bash butil.sh -v tornadovm -d 0 stories110M.bin 2>&1 | tee -a $logFolder/log110_tornadovm_0
 
 ## Level Zero Device 1
-bash butil.sh -v tornadovm -d 1 stories15M.bin 2> $logFolder/log15_tornadovm_1
-bash butil.sh -v tornadovm -d 1 stories42M.bin 2> $logFolder/log42_tornadovm_1
-bash butil.sh -v tornadovm -d 1 stories110M.bin 2> $logFolder/log110_tornadovm_1
+bash butil.sh -v tornadovm -d 1 stories15M.bin 2>&1  | tee -a $logFolder/log15_tornadovm_1
+bash butil.sh -v tornadovm -d 1 stories42M.bin 2>&1  | tee -a $logFolder/log42_tornadovm_1
+bash butil.sh -v tornadovm -d 1 stories110M.bin 2>&1 | tee -a $logFolder/log110_tornadovm_1
 
