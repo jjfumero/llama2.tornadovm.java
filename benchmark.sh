e34067b2fb1d4f29b9760108b68b1b810f774208
Juan Fumero <jjfumero@gmail.com>
2024-10-03 14:48:45 +0200

    minor change

--- benchmark.sh
+++ benchmark.sh
@@ -7,9 +7,9 @@ bash butil.sh -v java stories42M.bin 2> $logFolder/log42_java
 bash butil.sh -v java stories110M.bin 2> $logFolder/log110_java
 
 ## Level Zero Device 0
-bash butil.sh -v levelzero stories15M.bin 2> $logFolder/log15_levelzero_0
-bash butil.sh -v levelzero stories42M.bin 2> $logFolder/log42_levelzero_0
-bash butil.sh -v levelzero stories110M.bin 2> $logFolder/log110_levelzero_0
+bash butil.sh -v levelzero -d 0 stories15M.bin 2> $logFolder/log15_levelzero_0
+bash butil.sh -v levelzero -d 0 stories42M.bin 2> $logFolder/log42_levelzero_0
+bash butil.sh -v levelzero -d 0 stories110M.bin 2> $logFolder/log110_levelzero_0
 
 ## Level Zero Device 1
 bash butil.sh -v levelzero -d 1 stories15M.bin 2> $logFolder/log15_levelzero_1
@@ -18,9 +18,9 @@ bash butil.sh -v levelzero -d 1 stories110M.bin 2> $logFolder/log110_levelzero_1
 
 
 ## TornadoVM Device 0
-bash butil.sh -v tornadovm stories15M.bin 2> $logFolder/log15_tornadovm_0
-bash butil.sh -v tornadovm stories42M.bin 2> $logFolder/log42_tornadovm_0
-bash butil.sh -v tornadovm stories110M.bin 2> $logFolder/log110_tornadovm_0
+bash butil.sh -v tornadovm -d 0 stories15M.bin 2> $logFolder/log15_tornadovm_0
+bash butil.sh -v tornadovm -d 0 stories42M.bin 2> $logFolder/log42_tornadovm_0
+bash butil.sh -v tornadovm -d 0 stories110M.bin 2> $logFolder/log110_tornadovm_0
 
 ## Level Zero Device 1
 bash butil.sh -v tornadovm -d 1 stories15M.bin 2> $logFolder/log15_tornadovm_1
